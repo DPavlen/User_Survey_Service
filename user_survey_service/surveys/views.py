4120d90e88c8355edca6914bc585d54a3894df2e
Dmitry Pavlenko <125051658+DPavlen@users.noreply.github.com>
2024-02-04 20:35:34 +0300

    Вывод всех Опросов в index.html

--- user_survey_service/surveys/views.py
+++ user_survey_service/surveys/views.py
@@ -1,17 +1,23 @@
 from django.contrib.auth.decorators import login_required
-from django.views import View
+from django.core.paginator import Paginator
+# from django.views import View
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 
 from surveys.models import Survey, Question, Answer
+# from surveys.utils import paginator_context
 
 COUNT_POST = 10
+
 
 def index(request):
     """Главная страница списка опросов пользователей."""
     surveys = Survey.objects.all()
+    paginator = Paginator(surveys, COUNT_POST)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'Опросы': surveys
-        }
+        'page_obj': page_obj,
+    }
     print(surveys)
     return render(request, "surveys/index.html", context )
